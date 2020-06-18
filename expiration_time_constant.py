a8d3b28d4f51c316b036f27b8a42a0f34ea67b67
fra-mar <martinez.torrente@gmail.com>
2020-06-18 10:31:31 +0200

    One more file

---
+++ expiration_time_constant.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+
+v0=450
+tau=0.7
+tmax=3
+
+t=np.linspace(0,tmax,100)
+v=v0* np.exp(-t/tau)
+
+fig=plt.figure()
+ax=fig.add_subplot(1,1,1)
+
+ax.plot(t,v)
+ax.hlines(0,xmin=0, xmax=tmax,ls=':',color='g')
+
+ntau=tmax//tau +1
+xt=np.arange(0,ntau,step=1,dtype='int')
+ax.set_xticks(xt*tau)
+ax.set_xticklabels(xt)
+
+
+
+secax = ax.secondary_xaxis('top')  #look documentation
+#secax.
+secax.set_xlabel('Time in seconds')
+ax.set_xlabel('n x tau')
+
+plt.vlines(tau,ymin=-0.4,ymax=v0* np.exp(-tau/tau))
+plt.hlines(v0* np.exp(-tau/tau),0,tau)
+plt.show()
+
+
+#%%
+stacked=np.stack((t,v),axis=1)
+plt.plot(stacked[:,0],stacked[:,1])  #and you get the same...
+
+
+
